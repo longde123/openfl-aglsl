b0cd2f3e4021ac926f79d80b161335d54436920a
paling <huangchunbiao@gmail.com>
2014-01-04 12:24:23 +0800

    openfl-aglsl-test

---
+++ Main.hx
@@ -0,0 +1,222 @@
+package ;
+
+
+import flash.geom.Vector3D;
+import flash.Lib;
+import flash.display.Stage3D;
+import flash.display.Sprite;
+import flash.display.StageAlign;
+import flash.display.StageScaleMode;
+import flash.display3D.Context3D;
+import flash.display3D.Context3DProgramType;
+import flash.display3D.Context3DVertexBufferFormat;
+import flash.display3D.IndexBuffer3D;
+import flash.display3D.Program3D;
+import flash.display3D.shaders.AGLSLShaderUtils;
+import flash.display3D.VertexBuffer3D;
+import flash.events.Event;
+import flash.geom.Matrix3D;
+import flash.utils.ByteArray;
+import flash.Vector.Vector;
+using OpenFLStage3D;
+import flash.display3D.shaders.Shader;
+
+#if (cpp || neko || js)
+import openfl.gl.GL; 
+#end
+/**
+ * ...
+ * @author 
+ */
+class Main extends Sprite
+{
+	
+	public static function myTrace(message : Dynamic, ?posInfos : haxe.PosInfos) : Void
+    {
+        #if flash
+            flash.Lib.trace(message);
+        #elseif js
+             untyped console.log(message);
+        #else
+            untyped __trace(message, posInfos);
+        #end
+
+    }
+	public static function main() 
+	{
+		
+
+		//haxe.Log.trace = myTrace;
+		// static entry point
+		Lib.current.stage.align = flash.display.StageAlign.TOP_LEFT;
+		Lib.current.stage.scaleMode = flash.display.StageScaleMode.NO_SCALE;
+		Lib.current.addChild(new Main());
+	}
+ 
+	private var context3D:Context3D;
+	private var vertexbuffer:VertexBuffer3D;
+	private var indexBuffer:IndexBuffer3D; 
+	private var program:Program3D; 
+	private var stage3D : Stage3D;
+	public function new() 
+	{
+		super();
+		
+		
+		
+		var stage = Lib.current.stage;
+		stage.scaleMode = StageScaleMode.NO_SCALE;
+		stage.align = StageAlign.TOP_LEFT; 
+		
+		stage3D = stage.getStage3D(0);
+        stage3D.addEventListener(Event.CONTEXT3D_CREATE, init); 
+        stage3D.requestContext3D(); 
+			 
+	}
+	
+	private function init(e:Event):Void
+	{
+		
+		context3D =stage3D.context3D;	
+		
+		context3D.configureBackBuffer(800, 480, 1, false); 
+		var vertexData:Array<Float> = [-0.3,-0.3,0, 1, 0, 0, // x, y, z, r, g, b
+				-0.3, 0.3, 0, 0, 1, 0,
+				0.3, 0.3, 0, 0, 0, 1];
+		var indexData:Array<UInt> = [0, 1, 2];
+		program = context3D.createProgram(); 
+		// 3 vertices, of 6 Numbers each
+		vertexbuffer = context3D.createVertexBuffer(3, 6); 
+		vertexbuffer.uploadFromVector(flash.Vector.ofArray(vertexData), 0, 3);
+		 
+		indexBuffer = context3D.createIndexBuffer(3);			
+	 
+		
+		// offset 0, count 3
+		indexBuffer.uploadFromVector(flash.Vector.ofArray(indexData), 0, 3);
+			
+	  #if (flash)
+		var vertexShaderAssembler : Shader = AGLSLShaderUtils.createShader( Context3DProgramType.VERTEX,
+			"m44 op, va0, vc0\n" +
+			"mov v0, va1\n"
+		);
+		var fragmentShaderAssembler : Shader= AGLSLShaderUtils.createShader(Context3DProgramType.FRAGMENT,
+			"mov oc, v0\n"
+		);
+			#end
+		  #if (cpp || neko || js)
+		var vertexShaderAssembler : Shader = createWebGLShader( Context3DProgramType.VERTEX,
+				"precision mediump float;
+				uniform float yflip;
+				uniform vec4 vc0;
+				uniform vec4 vc1;
+				uniform vec4 vc2;
+				uniform vec4 vc3;
+				attribute vec4 va0;
+				attribute vec4 va1;
+				varying vec4 vi0;
+				vec4 outpos;
+				void main() {
+				  outpos.x = float(dot(vec4(va0),vec4(vc0)));
+				  outpos.y = float(dot(vec4(va0),vec4(vc1)));
+				  outpos.z = float(dot(vec4(va0),vec4(vc2)));
+				  outpos.w = float(dot(vec4(va0),vec4(vc3)));
+				  vi0 = vec4(va1);
+				  gl_Position = vec4(outpos.x, yflip*outpos.y, outpos.z*2.0 - outpos.w, outpos.w);
+				}"
+		);
+		var fragmentShaderAssembler : Shader= createWebGLShader(Context3DProgramType.FRAGMENT,
+			"precision mediump float;
+			varying vec4 vi0;
+			void main() {
+			  gl_FragColor = vec4(vi0);
+			}"
+		);
+		#end
+		
+		program.upload( vertexShaderAssembler, fragmentShaderAssembler);
+	
+		context3D.setRenderCallback(onRender);
+		
+		rot = 0;
+	}
+	
+	private var rot:Float;
+	private function onRender(e:Event):Void
+	{
+		//return;
+		if (context3D==null) 
+			return;
+				
+		rot += .5;
+		if (rot >= 360) rot = 1;
+ 
+	
+	
+		 
+        context3D.setProgram(program);
+       
+		 
+
+		// vertex position to attribute register 0
+		context3D.setVertexBufferAt (0, vertexbuffer, 0, Context3DVertexBufferFormat.FLOAT_3);
+		// uv coordinates to attribute register 1
+		context3D.setVertexBufferAt(1, vertexbuffer, 3, Context3DVertexBufferFormat.FLOAT_3);	
+		// assign shader program
+	 
+		
+		var matrix3D:Matrix3D = new Matrix3D();    
+	 
+		matrix3D.identity(); 
+		//matrix3D.appendRotation(Lib.getTimer()/40, Vector3D.Z_AXIS);
+		//matrix3D.appendRotation(rot, Vector3D.Z_AXIS);    
+		
+		context3D.clear (1, 0, 0, 1);
+	 
+		context3D.setProgramConstantsFromMatrix(Context3DProgramType.VERTEX, 0, matrix3D, false);
+		context3D.drawTriangles(indexBuffer, 0, 1); 
+	 
+		 
+		context3D.present();
+	}
+	
+	   #if (cpp || neko || js)
+	inline public static function createWebGLShader (type: Context3DProgramType, shaderSource:String): flash.display3D.shaders.Shader {
+
+     
+
+		 
+	
+		var glType : Int; 
+        switch(type){
+            case Context3DProgramType.VERTEX: {
+				glType = GL.VERTEX_SHADER; 
+			}
+            case Context3DProgramType.FRAGMENT: {
+				glType = GL.FRAGMENT_SHADER; 
+			}
+        }
+
+		 
+		var shader = GL.createShader (glType);
+		GL.shaderSource (shader, shaderSource);
+		GL.compileShader (shader);
+
+		if (GL.getShaderParameter (shader, GL.COMPILE_STATUS) == 0) {
+
+			trace("--- ERR ---\n" + shaderSource);
+			var err = GL.getShaderInfoLog (shader);
+			if (err != "") throw err;
+
+		} 
+		 
+		return shader;
+
+	 
+
+   
+
+    }
+     #end
+}
+
